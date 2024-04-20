95785b8b7097da327a5121d76cba54dcc50b5c71
LJHJH <160108023+JHL222@users.noreply.github.com>
2024-04-20 19:38:34 +0900

    Github Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,172 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import './App.css';
+import PokemonModal from './PokemonModal';
+
+const typeNames = {
+  normal: '노말',
+  fighting: '격투',
+  flying: '비행',
+  poison: '독',
+  ground: '땅',
+  rock: '바위',
+  bug: '벌레',
+  ghost: '고스트',
+  steel: '강철',
+  fire: '불',
+  water: '물',
+  grass: '풀',
+  electric: '전기',
+  psychic: '에스퍼',
+  ice: '얼음',
+  dragon: '드래곤',
+  dark: '악',
+  fairy: '페어리'
+};
+
+const getTypeNameInKorean = (type) => {
+  return typeNames[type] || type;
+};
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [shinyMode, setShinyMode] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const observer = useRef();
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  useEffect(() => {
+    if (!loading && hasMore) {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 1.0
+      };
+      observer.current = new IntersectionObserver(handleObserver, options);
+      if (observer.current) {
+        observer.current.observe(document.getElementById('observer'));
+      }
+    }
+  }, [loading, hasMore]);
+
+  const handleObserver = (entities, observer) => {
+    const target = entities[0];
+    if (target.isIntersecting && hasMore) {
+      observer.unobserve(target.target);
+      fetchPokemon();
+    }
+  };
+
+  const fetchPokemon = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=50`);
+      const results = response.data.results;
+      const newPokemonList = await Promise.all(results.map(async (pokemon) => {
+        const detailsResponse = await axios.get(pokemon.url);
+        const speciesResponse = await axios.get(detailsResponse.data.species.url);
+        const koreanName = speciesResponse.data.names.find(name => name.language.name === "ko").name;
+        const typesInKorean = detailsResponse.data.types.map(type => getTypeNameInKorean(type.type.name));
+        return { ...detailsResponse.data, imageNumber: parseInt(pokemon.url.split("/")[6]), koreanName, typesInKorean };
+      }));
+
+      let filteredPokemonList = newPokemonList;
+  
+      setPokemonList((prevPokemonList) => {
+        const uniquePokemonList = [...prevPokemonList, ...filteredPokemonList];
+        return uniquePokemonList.filter((pokemon, index, self) =>
+          index === self.findIndex((p) => p.id === pokemon.id)
+        );
+      });
+  
+      if (results.length < 50) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error('Error fetching Pokemon:', error);
+    }
+    setLoading(false);
+    setOffset(pokemonList.length);
+  };
+
+  const toggleShinyMode = () => {
+    setShinyMode(!shinyMode);
+  };
+
+  const toggleHomeMode = () => {
+    setShinyMode(false);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handlePokemonClick = (pokemon) => {
+    setSelectedPokemon(pokemon);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <ul>
+          <h1>Pokemon Dictionary</h1>
+          <li><a href="/" onClick={toggleHomeMode}>Home</a></li>
+          <li><a onClick={toggleShinyMode}>{shinyMode ? 'Shiny' : 'Normal'}</a></li>
+          <li>
+            <input
+              type="text"
+              className='search'
+              placeholder="Search Pokemon"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </li>
+        </ul>
+      </nav>
+     
+      <div className="pokemon-container">
+        {pokemonList
+          .filter(pokemon => {
+            return pokemon.koreanName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                   pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+          })
+          .map((pokemon, index) => (
+            <div key={index} className="pokemon-wrapper" onClick={() => handlePokemonClick(pokemon)}>
+              <div className="pokemon-info">
+                <small>#{pokemon.id}</small>
+                <img 
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${shinyMode ? 'shiny/' : ''}${pokemon.imageNumber}.png`}
+                  alt={pokemon.name} 
+                />
+                <p className='name'>{pokemon.koreanName ? pokemon.koreanName : pokemon.name}</p>
+                <p>타입: {pokemon.typesInKorean.join(', ')}</p> 
+              </div>
+            </div>
+          ))}
+        <div id="observer"></div>
+      </div>
+      
+      {!loading && hasMore && <p></p>}
+      
+      {modalOpen && selectedPokemon && (
+        <PokemonModal pokemon={selectedPokemon} onClose={closeModal} />
+      )}
     </div>
   );
 }
---
+++ src/PokemonModal.js
@@ -0,0 +1,63 @@
+// PokemonModal.js
+import React from 'react';
+import './PokemonModal.css'; 
+
+const PokemonModal = ({ pokemon, onClose }) => {
+  const calculateTotalStats = () => {
+    const stats = ["hp", "attack", "defense", "special-attack", "special-defense", "speed"];
+    let total = 0;
+    for (const statName of stats) {
+      total += pokemon.stats.find(stat => stat.stat.name === statName).base_stat;
+    }
+    return total;
+  };
+
+  const getBaseStatValue = (statName) => {
+    return pokemon.stats.find(stat => stat.stat.name === statName).base_stat;
+  };
+
+  const renderStatBar = (statName) => {
+    const value = getBaseStatValue(statName);
+    const maxValue = 255;
+    const percentage = (value / maxValue) * 100;
+    return (
+      <div className="stat-bar">
+        <div className="stat-bar-fill" style={{ width: `${percentage}%` }}></div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <p>#{pokemon.id}</p>
+        <h2>{pokemon.koreanName ? pokemon.koreanName : pokemon.name}</h2>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.imageNumber}.gif`} 
+          alt={pokemon.name}
+          className="pokemon-image"
+        />
+        <p>타입: {pokemon.typesInKorean.join(', ')}</p>
+        <p>기술: {pokemon.abilities.map(ability => ability.ability.name).join(', ')}</p>
+        <div className="stats">
+          <p>체력: {getBaseStatValue("hp")}</p>
+          {renderStatBar("hp")}
+          <p>공격: {getBaseStatValue("attack")}</p>
+          {renderStatBar("attack")}
+          <p>방어: {getBaseStatValue("defense")}</p>
+          {renderStatBar("defense")}
+          <p>특수공격: {getBaseStatValue("special-attack")}</p>
+          {renderStatBar("special-attack")}
+          <p>특수방어: {getBaseStatValue("special-defense")}</p>
+          {renderStatBar("special-defense")}
+          <p>속도: {getBaseStatValue("speed")}</p>
+          {renderStatBar("speed")}
+        </div>
+        <p>합계: {calculateTotalStats()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonModal;
